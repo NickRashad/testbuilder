602e0d517abbdc6202fbb5e9d4224236e0d90552
NickRashad <nrsternes@gmail.com>
2018-12-13 20:03:59 -0600

    Fix naming convention to return correct bank

--- detectNetwork.js
+++ detectNetwork.js
@@ -11,12 +11,13 @@ var detectNetwork = function(cardNumber) {
   // Note: `cardNumber` will always be a string
   // The Diner's Club network always starts with a 38 or 39 and is 14 digits long
   // The American Express network always starts with a 34 or 37 and is 15 digits long
-  if(cardNumber.length === 14 && (cardNumber.slice(0,2) === '38' || cardNumber.slice(0,2) === '39')){
-    return 'The Diner\'s Club';
-  }else if(cardNumber.length === 15 && (cardNumber.slice(0,2) === '34' || cardNumber.slice(0,2) === '37')){
-  	return 'The American Express';
+  var prefix = cardNumber.slice(0,2).toString();
+  var len = cardNumber.length;
+  if(len === 14 && (prefix === '38' || prefix === '39')){
+    return "Diner\'s Club";
+  } else if (len=== 15 && (prefix === '34' || prefix === '37')){
+  	return "American Express";
   }
   // Once you've read this, go ahead and try to implement this function, then return to the console.
 };
-
 
